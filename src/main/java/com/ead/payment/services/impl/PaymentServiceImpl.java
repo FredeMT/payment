1e2cddbb7a125980ae2cd566d55d81d5dd369f52
FredeBr <fredebrx@gmail.com>
2024-01-14 10:41:54 -0400

    Request and Get payment methods with command message

--- src/main/java/com/ead/payment/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/ead/payment/services/impl/PaymentServiceImpl.java
@@ -1,8 +1,91 @@
 package com.ead.payment.services.impl;
 
+import com.ead.payment.dtos.PaymentCommandDto;
+import com.ead.payment.dtos.PaymentRequestDto;
+import com.ead.payment.enums.PaymentControl;
+import com.ead.payment.models.CreditCardModel;
+import com.ead.payment.models.PaymentModel;
+import com.ead.payment.models.UserModel;
+import com.ead.payment.publishers.PaymentCommandPublisher;
+import com.ead.payment.repositories.CreditCardRepository;
+import com.ead.payment.repositories.PaymentRepository;
 import com.ead.payment.services.PaymentService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private static final Logger logger = LogManager.getLogger(PaymentServiceImpl.class);
+
+    @Autowired
+    CreditCardRepository creditCardRepository;
+
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    @Autowired
+    PaymentCommandPublisher paymentCommandPublisher;
+
+    public PaymentModel requestPayment(PaymentRequestDto paymentRequestDto, UserModel userModel) {
+        var creditCardModel = new CreditCardModel();
+        var creditCardModelOptional = creditCardRepository.findByUser(userModel);
+        if(creditCardModelOptional.isPresent()){
+            creditCardModel = creditCardModelOptional.get();
+        }
+        //Save CreditCardModel.
+        BeanUtils.copyProperties(paymentRequestDto, creditCardModel);
+        creditCardModel.setUser(userModel);
+        creditCardRepository.save(creditCardModel);
+
+        //save PaymentModel.
+        var paymentModel = new PaymentModel();
+        paymentModel.setPaymentControl(PaymentControl.REQUESTED);
+        paymentModel.setPaymentRequestDate(LocalDateTime.now(ZoneId.of("UTC")));
+        paymentModel.setPaymentExpirationDate(LocalDateTime.now(ZoneId.of("UTC")).plusDays(30));
+        paymentModel.setLastDigitsCreditCard(paymentRequestDto.getCreditCardNumber().substring(paymentRequestDto.getCreditCardNumber().length()-4));
+        paymentModel.setValuePaid(paymentRequestDto.getValuePaid());
+        paymentModel.setUser(userModel);
+        paymentRepository.save(paymentModel);
+
+        //send request to queue.
+        try{
+            var paymentCommandDto = new PaymentCommandDto();
+            paymentCommandDto.setUserId(userModel.getUserId());
+            paymentCommandDto.setPaymentId(paymentModel.getPaymentId());
+            paymentCommandDto.setCardId(creditCardModel.getCardId());
+            paymentCommandPublisher.publishPaymentCommand(paymentCommandDto);
+        }   catch (Exception e) {
+            logger.warn("Error sending payment command!");
+        }
+        return paymentModel;
+    }
+
+    @Transactional
+    @Override
+    public Optional<PaymentModel> findLastPaymentByUser(UserModel userModel) {
+        return paymentRepository.findTopByUserOrderByPaymentRequestDateDesc(userModel);
+    }
+
+    @Override
+    public Page<PaymentModel> findAllByUser(Specification<PaymentModel> spec, Pageable pageable) {
+        return paymentRepository.findAll(spec, pageable);
+    }
+
+    @Override
+    public Optional<PaymentModel> findPaymentByUser(UUID userId, UUID paymentId) {
+        return  paymentRepository.findPaymentByUser(userId, paymentId);
+    }
 }
