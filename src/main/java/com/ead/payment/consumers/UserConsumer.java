97d98adf3701f9b6c7581e77fee7a574fd1d3192
FredeBr <fredebrx@gmail.com>
2024-01-22 19:38:27 -0400

    Propagação de PaymentEvent pela Arquitetura

--- src/main/java/com/ead/payment/consumers/UserConsumer.java
+++ src/main/java/com/ead/payment/consumers/UserConsumer.java
@@ -3,6 +3,7 @@ package com.ead.payment.consumers;
 import com.ead.payment.dtos.UserEventDto;
 import com.ead.payment.enums.ActionType;
 import com.ead.payment.enums.PaymentStatus;
+import com.ead.payment.models.UserModel;
 import com.ead.payment.services.UserService;
 import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.rabbit.annotation.Exchange;
@@ -23,14 +24,14 @@ public class UserConsumer {
             value = @Queue(value = "${ead.broker.queue.userEventQueue.name}", durable = "true"),
             exchange = @Exchange(value = "${ead.broker.exchange.userEventExchange}", type = ExchangeTypes.FANOUT, ignoreDeclarationExceptions = "true")))
     public void listenUserEvent(@Payload UserEventDto userEventDto) {
-        var userModel = userEventDto.convertToUserModel();
         switch (ActionType.valueOf(userEventDto.getActionType())){
             case CREATE:
+                var userModel = userEventDto.convertToUserModel(new UserModel());
                 userModel.setPaymentStatus(PaymentStatus.NOTSTARTED);
                 userService.save(userModel);
                 break;
             case UPDATE:
-                userService.save(userModel);
+                userService.save(userEventDto.convertToUserModel(userService.findById(userEventDto.getUserId()).get()));
                 break;
             case DELETE:
                 userService.delete(userEventDto.getUserId());
